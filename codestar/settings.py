4ce12f1dda6f148ef3d40b7f03edd45da1db96cc
Geoff Fletcher <xov4m05a@students.codeinstitute.net>
2024-12-11 10:37:30 +0000

    added comment CRUD

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-geofffletcher-blog-w4tarn3gnul.ws.codeinstitute-ide.net",'.herokuapp.com']
 
